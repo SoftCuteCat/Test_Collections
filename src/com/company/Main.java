a4f73e9433a881120570d66ae9081b5a24e8a973
evgeny <kalishkinwork@gmail.com>
2021-12-08 08:14:20 +0300

    Final version

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,8 +8,15 @@ import com.company.PerformanceTester;
 public class Main {
 
     public static void main(String[] args) {
+        System.out.println("Please enter the number of checks.");
 
-            PerformanceTester test = new PerformanceTester(100000);
-            System.out.println(test.toString());
+        Scanner scanInt = new Scanner(System.in);
+        while (!scanInt.hasNextInt()) {
+            System.out.println("Enter positive number, please!");
+            scanInt.next();
+        }
+        int countOfMethodCalls = scanInt.nextInt();
+        PerformanceTester tester = new PerformanceTester(countOfMethodCalls);
+        System.out.println(tester.toString());
     }
 }
--- src/com/company/PerformanceTester.java
+++ src/com/company/PerformanceTester.java
@@ -4,17 +4,29 @@ import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 
+/**
+ *
+ * @author evgeniy-
+ * PerfomanceTester is class where we calculate and compare perfomance.
+ */
 public class PerformanceTester {
     private int countOfMethodCalls;
     private int [][] CollectionsRunningTime;
 
+
+    /**
+     * PefromanceTester is a constructor which creates object tester,
+     * it also creates table of time for list and and array
+     */
     public PerformanceTester(int countOfMethodCalls){
         this.countOfMethodCalls = countOfMethodCalls;
         CollectionsRunningTime = new int[2][3];
         GeneralTester();
-
     }
-
+    /**
+     *GeneralTester creates array of indices, it contains creating indices
+     * for methods get, add, delete. In total we create table of results
+     */
     private void GeneralTester(){
         int [][] arrayIndices = new int[3][];
         arrayIndices[0] = createArrayIndicesForAdd();
@@ -24,7 +36,13 @@ public class PerformanceTester {
         ListTester(0, new ArrayList<Integer>(), arrayIndices);
         ListTester(1, new LinkedList<Integer>(),arrayIndices);
     }
-
+    /**
+     *ListTester is method which tests methods for both linkedlist and array, first starting from testing array. We aplay each method to array, begining from add, then get
+     *and then delete. We use cycles for in order to apply method.
+     @param indexOfContainer index of first length where we use array
+     @param list is list-container with numbers
+     @param array is array-container with numbers
+     */
     private void ListTester(int indexOfContainer, List<Integer> list, int[][] array ) {
         long startAdd = System.currentTimeMillis();
         for (int i = 0; i < countOfMethodCalls; ++i) {
@@ -44,10 +62,14 @@ public class PerformanceTester {
         }
         CollectionsRunningTime[indexOfContainer][2] =  (int)(System.currentTimeMillis() - startDelete);
     }
+
     public int getCountOfMethodCalls(){
         return countOfMethodCalls;
     }
 
+    /**
+     *creating array of indices for add method. (random indices)
+     */
     private int[] createArrayIndicesForAdd(){
         int[] arrayIndicesForAdd = new int[countOfMethodCalls];
         for (int i = 0; i < countOfMethodCalls; ++i){
@@ -55,7 +77,9 @@ public class PerformanceTester {
         }
         return arrayIndicesForAdd;
     }
-
+    /**
+     *creating array of indices for get method. (random indices)
+     */
     private int[] createArrayIndicesForGet(){
         int[] arrayIndicesForGet = new int[countOfMethodCalls];
         for (int i = 0; i < countOfMethodCalls; ++i){
@@ -63,8 +87,9 @@ public class PerformanceTester {
         }
         return  arrayIndicesForGet;
     }
-
-
+    /**
+     *creating array of indices for remove method. (random indices)
+     */
     private int[] createArrayIndicesForDelete(){
         int[] arrayIndicesForDelete = new int[countOfMethodCalls];
         for (int i = 0; i < countOfMethodCalls; ++i){
@@ -72,7 +97,9 @@ public class PerformanceTester {
         }
         return arrayIndicesForDelete;
     }
-
+    /**
+     *overriding equals method which would compare container between
+     */
     @Override
     public boolean equals(Object obj) {
         if (obj == null)
@@ -82,6 +109,9 @@ public class PerformanceTester {
         return that.toString().equals(this.toString()) &&
                 that.countOfMethodCalls == this.countOfMethodCalls;
     }
+    /**
+     *overriding equals method which would allowing us to get hash of an element
+     */
     @Override
     public int hashCode() {
         int hash = 0;
@@ -92,18 +122,20 @@ public class PerformanceTester {
         }
         return hash;
     }
-
+    /**
+     *overriding tostring method
+     */
     @Override
     public String toString() {
         //String resultTable = "";
         return "Array List:  " +
                         "add: " + String.valueOf(CollectionsRunningTime[0][0])  + " " +
                         "get: " + String.valueOf(CollectionsRunningTime[0][1])  + " " +
-                        "delete:" + String.valueOf(CollectionsRunningTime[0][2])  + "\n" +
+                        "remove:" + String.valueOf(CollectionsRunningTime[0][2])  + "\n" +
                         "Linked List:  " +
                         "add: " + String.valueOf(CollectionsRunningTime[1][0])  + " " +
                         "get: " + String.valueOf(CollectionsRunningTime[1][1])  + " " +
-                        "delete: " + String.valueOf(CollectionsRunningTime[1][2]);
+                        "remove: " + String.valueOf(CollectionsRunningTime[1][2]);
     }
 
 }
